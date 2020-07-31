1756c982065e9cb1063d0fe930c196507453141f
FlagGuarder <34416290+FlagGuarder@users.noreply.github.com>
2020-07-31 12:20:00 +0800

    Update Solution.java

--- algorithm/src/Array/Solution53/Solution.java
+++ algorithm/src/Array/Solution53/Solution.java
@@ -1,8 +1,8 @@
 package Array.Solution53;
 
 public class Solution {
-    //  面试题 08.03. Magic Index LCCI  魔术索引
-    // https://leetcode-cn.com/problems/magic-index-lcci/
+    //  [面试题 08.03. Magic Index LCCI  魔术索引](https://leetcode-cn.com/problems/magic-index-lcci/)
+    
     public int findMagicIndex(int[] nums) {
         //没难度的题，基本就是比较，找到返回，没找到就返回-1，没了，惊不惊喜，意不意外
         int n = nums.length;
